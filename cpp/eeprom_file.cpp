8dbf1dce1c2909fd5459cd2b56b3cb2e4527855b
barthess <barthess@yandex.ru>
2014-09-01 10:49:14 +0300

    Fixed license

--- cpp/eeprom_file.cpp
+++ cpp/eeprom_file.cpp
@@ -1,13 +1,23 @@
 /*
+/*
     Abstraction layer for EEPROM ICs.
 
-    Copyright (C) 2012 Uladzimir Pylinski aka barthess
+    Copyright (C) 2012,2013,2014 Uladzimir Pylinski aka barthess
 
-    Licensed under the 3-Clause BSD license, (the "License");
-    you may not use this file except in compliance with the License.
-    You may obtain a copy of the License at
+    This file is part of 24AA lib.
 
-        http://directory.fsf.org/wiki/License:BSD_3Clause
+    24AA lib is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    24AA lib is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include "ch.hpp"
